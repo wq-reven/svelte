5453aa93f76a3f0c426239c750c7781778243963
wangqiang <wangqiang5@jiandan100.cn>
2021-04-23 17:54:44 +0800

    添加文件

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+ import Example1 from "./Example1.svelte";
+</script>
+
+<div>
+  <Example1 />
+</div>
---
+++ src/Example1.svelte
@@ -0,0 +1,24 @@
+<script>
+  let count = 0
+</script>
+
+ <p>clicked {count} times</p>
+<button on:click={() =>{ count ++ }}>
+  Click
+</button>
+
+
+<!-- import React, { useState } from 'react';
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click
+      </button>
+    </div>
+  );
+} -->
